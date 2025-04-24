9bd74c09def43940c44c7318467fc90f3f154c0b
Edwin Peña <edwinsp095@gmail.com>
2025-04-24 08:26:39 -0500

    feat phonebook backend step 7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-
-app.use(express.json());
+const morgan = require("morgan");
 
 let persons = [
   {
@@ -25,6 +24,9 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+app.use(express.json());
+app.use(morgan("tiny"));
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
