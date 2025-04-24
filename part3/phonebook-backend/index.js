c9aefab8eca93b69cdf617f73d9c7ac387d0491b
Edwin Peña <edwinsp095@gmail.com>
2025-04-24 13:02:15 -0500

    feat phonebook backend step 8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -25,8 +25,13 @@ let persons = [
   },
 ];
 
+morgan.token("body", (request, response) => {
+  return JSON.stringify(request.body);
+});
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
